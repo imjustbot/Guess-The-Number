def99e75da6d74e10ef1655f90f09eee663ac972
imjustbot <nico7tolu@gmail.com>
2026-06-26 12:41:33 +0200

    Aggiornato il numero di numeri possibili

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import random
 import time
 
 
-numeri_possibili = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+numeri_possibili = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
 
 if "segreto" not in st.session_state:
-    st.session_state.segreto = random.randint(1, 15)
+    st.session_state.segreto = random.randint(1, 50)
 
 
-st.title("Indovina il numero (da 1 a 15)")
+st.title("Indovina il numero (da 1 a 100)")
 
 risposta = st.selectbox("NUMERO SEGRETO:", numeri_possibili, index=None, placeholder="Scegli il numero...", key=st.session_state.segreto)
 
